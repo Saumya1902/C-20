6390e49b93588a03ca4481c3b937f32ec543d0f4
Saumya1902 <69415157+Saumya1902@users.noreply.github.com>
2020-08-15 10:52:11 +0530

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,28 @@
+function setup() {
+  createCanvas(800,400);
+   mr=createSprite(400, 200, 50, 50);
+  mr.shapeColor="blue";
+   fr=createSprite(300,150,50,50);
+  fr.shapeColor="blue";
+}
+function draw() {
+  background(255,255,255);
+
+  mr.x=mouseX;
+  mr.y=mouseY;
+  
+  if(mr.x-fr.x<=mr.width/2+fr.width/2 &&
+    fr.x-mr.x<=mr.width/2+fr.width/2 &&
+    mr.y-fr.y<=mr.height/2+fr.height/2 && 
+    fr.y-mr.y<=mr.height/2+fr.height/2 )
+    {
+      mr.shapeColor="red";
+      fr.shapeColor="red";
+    }
+    else{
+      mr.shapeColor="blue";
+      fr.shapeColor="blue";
+    }
+
+  drawSprites();
+}
